2185a406b6a17b436244cf3e0312ceed36e0f7ae
Alin-Lipotchi <alinlipotchi1@gmail.com>
2023-05-09 22:50:17 +0300

    finished the functionality of the game

--- app.js
+++ app.js
@@ -3,12 +3,26 @@
 const checkBoxes = $(".check-box");
 const oTurn = $(".o-turn");
 const xTurn = $(".x-turn");
+const overlay = $(".overlay");
+const overlayBackground = $(".overlay-background");
+const result = $(".result");
 let locationArr = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
 ];
 let progress = 0;
+
+// Winning Conditions
+
+// Win Condition 0 - first line
+// Win Condition 1 - second line
+// Win Condition 2 - third line
+// Win Condition 3 - first column
+// Win Condition 4 - second column
+// Win Condition 5 - third column
+// Win Condition 6 - main diagonal
+// Win Condition 7 - secondary diagonal
 
 // Game Logic
 
@@ -30,8 +44,134 @@ checkBoxes.each((i, box) => {
             xTurn.css("opacity", "1");
             oTurn.css("opacity", "0");
         }
+        if (progress === 9) {
+            result.text("It's a Draw!");
+            displayOverlay();
+        } else if (progress > 4) {
+            // Win conditions
+
+            // Win Condition 0
+            if (
+                locationArr[0][0] !== 0 &&
+                locationArr[0][0] === locationArr[0][1] &&
+                locationArr[0][0] === locationArr[0][2]
+            ) {
+                if (locationArr[0][0] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 1
+            if (
+                locationArr[1][0] !== 0 &&
+                locationArr[1][0] === locationArr[1][1] &&
+                locationArr[1][0] === locationArr[1][2]
+            ) {
+                if (locationArr[1][0] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 2
+            if (
+                locationArr[2][0] !== 0 &&
+                locationArr[2][0] === locationArr[2][1] &&
+                locationArr[2][0] === locationArr[2][2]
+            ) {
+                if (locationArr[2][0] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 3
+            if (
+                locationArr[0][0] !== 0 &&
+                locationArr[0][0] === locationArr[1][0] &&
+                locationArr[0][0] === locationArr[2][0]
+            ) {
+                if (locationArr[0][0] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 4
+            if (
+                locationArr[0][1] !== 0 &&
+                locationArr[0][1] === locationArr[1][1] &&
+                locationArr[0][1] === locationArr[2][1]
+            ) {
+                if (locationArr[0][1] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 5
+            if (
+                locationArr[0][2] !== 0 &&
+                locationArr[0][2] === locationArr[1][2] &&
+                locationArr[0][2] === locationArr[2][2]
+            ) {
+                if (locationArr[0][2] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 6
+            if (
+                locationArr[0][0] !== 0 &&
+                locationArr[0][0] === locationArr[1][1] &&
+                locationArr[0][0] === locationArr[2][2]
+            ) {
+                if (locationArr[0][0] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+
+            // Win Condition 7
+            if (
+                locationArr[0][2] !== 0 &&
+                locationArr[0][2] === locationArr[1][1] &&
+                locationArr[0][2] === locationArr[2][0]
+            ) {
+                if (locationArr[0][2] === 1) {
+                    result.text("X has Won!");
+                } else {
+                    result.text("O has Won!");
+                }
+                displayOverlay();
+            }
+        }
     });
 });
+
+const displayOverlay = () => {
+    overlay.css("transform", "scale(1) translateY(0)");
+    overlayBackground.show();
+    setTimeout(() => {
+        overlayBackground.css("opacity", "1");
+    }, 1);
+};
 
 // Reset
 
@@ -58,4 +198,9 @@ const reset = () => {
     });
     xTurn.css("opacity", "1");
     oTurn.css("opacity", "0");
+    overlay.css("transform", "scale(0) translateY(500px)");
+    overlayBackground.css("opacity", "0");
+    setTimeout(() => {
+        overlayBackground.hide();
+    }, 500);
 };
